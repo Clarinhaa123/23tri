73594735433d73a2ad6f6a6ac22ff1c5a9a77176
Clarinhaa123 <126687333+Clarinhaa123@users.noreply.github.com>
2023-10-04 10:43:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-document.querySelector(".tecla_pom")
+function tocSsom (idElementoAudio) {
+    document.querySelector (idElementoAudio). paly();
+}
+
+const listaDeTeclas = document.querySelectorAll ('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla= listaDeTeclas [contador];
+    const instrumento = tecla.classlist[1];
+    const idAudio = `#som_${instrumento}` //template string 
+
+    tecla.onclick  = function () {
+        tocaSom (idAudio);
+    }
+
+}
+
+
